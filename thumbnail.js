e6a1e166400663b620ae85bf7e660340a28fc069
helloaycan <aycan.hussein@gmail.com>
2016-10-31 13:24:18 +0000

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,10 +1,37 @@
+
+<script language='javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js' type='text/javascript'/>
+<script type='text/javascript'>//<![CDATA[
 /**
 this script was written by Confluent Forms LLC http://www.confluentforms.com
 for the BlogXpertise website http://www.blogxpertise.com
 any updates to this script will be posted to BlogXpertise
 please leave this message and give credit where credit is due!
 **/
-      $(document).ready(function() {
+$(document).ready(function(){
+    // the dimension of your content within the columns
+    var areawidth = $('#Blog1').width();
+    $('.post-body').find('img').each(function(n, image){
+        var image = $(image);
+        var height = image.attr('height');
+        var width = image.attr('width');
+        var newHeight = (height/width * areawidth).toFixed(0);
+        image.attr('width',areawidth).attr('height',newHeight);
+        var greater = Math.max(areawidth,newHeight);
+        image.attr({src : image.attr('src').replace(/s\B\d{3,4}/,'s' + greater)});
+    });
+});
+//]]></script>
+
+
+
+
+/**
+this script was written by Confluent Forms LLC http://www.confluentforms.com
+for the BlogXpertise website http://www.blogxpertise.com
+any updates to this script will be posted to BlogXpertise
+please leave this message and give credit where credit is due!
+**/
+   <!--   $(document).ready(function() {
         var dimension = 280;
         $('.labelthumbnail').find('img').each(function(n, image){
           var image = $(image);
@@ -31,3 +58,4 @@ please leave this message and give credit where credit is due!
           image.attr('height',h);
         });
       });
+-->
