fb822752e7e105daad37858d0f8dc38c7d56f1dd
helloaycan <aycan.hussein@gmail.com>
2018-05-25 16:28:01 +0100

    Update littlewhitesocks.js

--- littlewhitesocks.js
+++ littlewhitesocks.js
@@ -57,6 +57,33 @@ return false;
 });
 });
 
+
+
+
+//<![CDATA[  
+$(document).ready(function() {
+var userFeed = new Instafeed({
+get: 'user',
+limit: "6",
+userId: 30746667,
+resolution: 'standard_resolution',
+square: 'true',
+accessToken: '30746667.3f33fc7.95ca36e07cbc4ccc86b3d02c4ccf56a6',
+clientId: '3f33fc7c3faf4ee2ba1b0ef93aaf2fbe',
+template: '<li><a href="{{link}}" target="_blank"> <div><img src="{{image}}" /></div></a></li>'
+});
+userFeed.run();
+});
+
+$(function() {
+$(document).ready(function() {
+$('.tweetable').each(function() {   
+$(this).wrap('<a class="tweet-link" target="_blank" href="https://twitter.com/intent/tweet?text=' + encodeURIComponent($(this).text()) + " " + encodeURIComponent($(this).parents('.post-outer').find('.timestamp-link').attr('href') || $(location).attr('href')) +'"></a>');
+    });
+});
+});
+//]]>
+
 $(document).ready(function () {
 $(".scroll-top-mobile, .scroll-top").click(function(event) {
     $('html, body').animate({
@@ -93,28 +120,3 @@ $(".scroll-top").css("display","block");
 } 
 });
  });
-
-
-//<![CDATA[  
-$(document).ready(function() {
-var userFeed = new Instafeed({
-get: 'user',
-limit: "6",
-userId: 30746667,
-resolution: 'standard_resolution',
-square: 'true',
-accessToken: '30746667.3f33fc7.95ca36e07cbc4ccc86b3d02c4ccf56a6',
-clientId: '3f33fc7c3faf4ee2ba1b0ef93aaf2fbe',
-template: '<li><a href="{{link}}" target="_blank"> <div><img src="{{image}}" /></div></a></li>'
-});
-userFeed.run();
-});
-
-$(function() {
-$(document).ready(function() {
-$('.tweetable').each(function() {   
-$(this).wrap('<a class="tweet-link" target="_blank" href="https://twitter.com/intent/tweet?text=' + encodeURIComponent($(this).text()) + " " + encodeURIComponent($(this).parents('.post-outer').find('.timestamp-link').attr('href') || $(location).attr('href')) +'"></a>');
-    });
-});
-});
-//]]>
