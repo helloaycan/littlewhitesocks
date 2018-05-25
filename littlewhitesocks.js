b80fc8ce70d8f1ef9ae57d5d3998017850154077
helloaycan <aycan.hussein@gmail.com>
2018-05-25 15:00:32 +0100

    Update littlewhitesocks.js

--- littlewhitesocks.js
+++ littlewhitesocks.js
@@ -56,11 +56,11 @@ return false;
 });
 });
 
-jQuery(function($) {
+
  $(".scroll-top-mobile, .scroll-top").click(function(event) {
     $('html, body').animate({
       scrollTop: 0
     }, 'slow');
     event.preventDefault();
   });
-});
+
